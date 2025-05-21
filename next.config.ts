e6c82857f9e52aedfcbee73b9e4303c035f24335
fantee-hub <tobifaniran@gmail.com>
2025-05-21 07:45:10 +0100

    feat: made some changes on the next.config file

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,10 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   images: {
-    domains: ["orooxuyvcuisiwhhlzih.supabase.co"],
+    domains: [
+      "orooxuyvcuisiwhhlzih.supabase.co",
+      "ioljpmwzlunhfrtecuyt.supabase.co",
+    ],
   },
 };
 
